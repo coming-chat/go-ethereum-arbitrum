7c9c08828244762241d0601e64767f918c7e8faf
xiang-xx <xiang9311@gmail.com>
2023-10-26 09:49:36 +0800

    feat: ignore retry to

--- core/types/transaction_marshalling.go
+++ core/types/transaction_marshalling.go
@@ -606,9 +606,9 @@ func (t *Transaction) UnmarshalJSON(input []byte) error {
 		if dec.Gas == nil {
 			return errors.New("missing required field 'gas' in txdata")
 		}
-		if dec.RetryTo == nil {
-			return errors.New("missing required field 'retryTo' in txdata")
-		}
+		// if dec.RetryTo == nil {
+		// 	return errors.New("missing required field 'retryTo' in txdata")
+		// }
 		if dec.Beneficiary == nil {
 			return errors.New("missing required field 'beneficiary' in transaction")
 		}
